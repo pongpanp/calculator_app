9721b88ea314c50241bec90fb2a48e67c1afe681
pongpanp <44573264+pongpanp@users.noreply.github.com>
2021-11-10 16:55:27 +0700

    properties

--- lib/widgets/calculator_button.dart
+++ lib/widgets/calculator_button.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CalculatorButton extends StatelessWidget {
-  const CalculatorButton({Key key}) : super(key: key);
+  const CalculatorButton({Key? key, required this.num}) : super(key: key);
+  final int num;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  TextButton(
+      onPressed: () {
+        print(num);
+      },
+      child:  SizedBox(
+        width: 70,
+        height: 70,
+        child: Center(
+            child: Text(
+              '$num',
+              style: const TextStyle(fontSize: 24.0, color: Colors.black),
+            )),
+      ),
+    );
   }
 }
